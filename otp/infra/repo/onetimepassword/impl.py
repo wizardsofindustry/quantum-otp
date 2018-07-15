4ecbf90a5ca11cc39a70f7b446b5e7279d660e12
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-15 06:00:00 +0000

    qsa: Update OneTimePasswordRepository documentation

--- otp/infra/repo/onetimepassword/impl.py
+++ otp/infra/repo/onetimepassword/impl.py
@@ -4,4 +4,7 @@ from .base import BaseOneTimePasswordRepository
 class OneTimePasswordRepository(BaseOneTimePasswordRepository):
 
     def persist_otp(self, persistable):
+        """Persists a One-Time Password (OTP) object to the persistent data
+        store.
+        """
         raise NotImplementedError("Subclasses must override this method.")
