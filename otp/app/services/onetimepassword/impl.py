fb2118dc9106b921bc8c0a5560155a30320e57dc
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-15 06:00:00 +0200

    Persist the generated QR-code

--- otp/app/services/onetimepassword/impl.py
+++ otp/app/services/onetimepassword/impl.py
@@ -9,11 +9,18 @@ class OneTimePasswordService(BaseOneTimePasswordService):
 
     def generate(self, kind, gsid, nsid, issuer):
         """Generates a new One-Time Password (OTP) for the identified Subject."""
-        if kind not in ('totp','hotp'):
+        if kind not in ('totp', 'hotp'):
             raise ValueError(f'Invalid OTP type: {kind}')
         secret = pyotp.random_base32()
         otp = getattr(pyotp, kind.upper())(secret)
         uri = otp.provisioning_uri(nsid, issuer_name=issuer)
+        dto = self.dto(
+            storage_class='otp',
+            gsid=gsid,
+            secret=secret,
+            counter=getattr(otp, 'interval', 0) # initialize counter at 0 if HOTP
+        )
+        self.repo.persist(dto)
         return self.dto(link=uri)
 
     def verify(self, kind, gsid, code):
