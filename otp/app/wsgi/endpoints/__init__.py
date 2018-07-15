f21b59df19f82f3b68f20da5b30348241c6741a0
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-15 06:00:00 +0000

    qsa: Compile endpoints from OpenAPI definition

--- otp/app/wsgi/endpoints/__init__.py
+++ otp/app/wsgi/endpoints/__init__.py
@@ -2,11 +2,13 @@ from werkzeug.routing import Map
 
 from .health import HealthEndpoint
 from .version import VersionEndpoint
+from .onetimepassword import OneTimePasswordEndpoint
 
 
 urlpatterns = Map([
     HealthEndpoint.as_rule(),
     VersionEndpoint.as_rule(),
+    OneTimePasswordEndpoint.as_rule(),
 ])
 
 
