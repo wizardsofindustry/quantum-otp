e98a5ce06b70512880a6ed1542c76ac890e1f65f
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-15 06:00:00 +0200

    Fix class docstring compiler bug

--- otp/app/services/onetimepassword/impl.py
+++ otp/app/services/onetimepassword/impl.py
@@ -5,9 +5,14 @@ from .base import BaseOneTimePasswordService
 
 
 class OneTimePasswordService(BaseOneTimePasswordService):
-    """Exposes an API to generate and verify One-Time Passwords (OTPs)."""Verified that the correct OTP was provided by the client, for the
-        identified Subject.
-        """
+    """Exposes an API to generate and verify One-Time Passwords (OTPs)."""
+    kinds = {
+        'totp': pyotp.TOTP,
+        'hotp': pyotp.HOTP
+    }
+
+    def generate(self, kind, gsid, nsid, issuer):
+        """Generates a new One-Time Password (OTP) for the identified Subject."""
         secret = pyotp.random_base32()
         otp = self.kinds[kind](secret)
         uri = otp.provisioning_uri(nsid, issuer_name=issuer)
