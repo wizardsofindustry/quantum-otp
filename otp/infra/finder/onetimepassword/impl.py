3b29adae266cdbd55093e3c5918f65515d70dcea
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-15 06:00:00 +0000

    qsa: Update OneTimePasswordFinderdocumentation

--- otp/infra/finder/onetimepassword/impl.py
+++ otp/infra/finder/onetimepassword/impl.py
@@ -4,4 +4,5 @@ from .base import BaseOneTimePasswordFinder
 class OneTimePasswordFinder(BaseOneTimePasswordFinder):
 
     def get_for_subject(self, gsid):
+        """Returns the shared secret for the Subject identified by `gsid`."""
         raise NotImplementedError("Subclasses must override this method.")
