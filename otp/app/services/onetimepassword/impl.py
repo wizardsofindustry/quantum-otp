3bf9add09f4785b82af2ca4906d52e292d0e194c
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-15 06:00:00 +0000

    qsa: Update OneTimePasswordService (app) documentation

--- otp/app/services/onetimepassword/impl.py
+++ otp/app/services/onetimepassword/impl.py
@@ -4,7 +4,11 @@ from .base import BaseOneTimePasswordService
 class OneTimePasswordService(BaseOneTimePasswordService):
 
     def generate(self, kind, gsid):
+        """Generates a new One-Time Password (OTP) for the identified Subject."""
         raise NotImplementedError("Subclasses must override this method.")
 
     def verify(self, kind, gsid, code):
+        """Verified that the correct OTP was provided by the client, for the
+        identified Subject.
+        """
         raise NotImplementedError("Subclasses must override this method.")
