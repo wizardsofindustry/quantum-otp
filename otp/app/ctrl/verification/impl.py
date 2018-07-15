4886c42c6db7543f467a21d7cba16f0ca4d9b4bd
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-15 06:00:00 +0200

    Implement VerificationCtrl.post()

--- otp/app/ctrl/verification/impl.py
+++ otp/app/ctrl/verification/impl.py
@@ -5,4 +5,5 @@ from .base import BaseVerificationCtrl
 class VerificationCtrl(BaseVerificationCtrl):
 
     async def post(self, request, *args, **kwargs):
-        raise NotImplementedError("Subclasses must override this method.")
+        self.otp.verify(kwargs.get('kind','totp'), **request.payload)
+        return self.render_to_response({})
