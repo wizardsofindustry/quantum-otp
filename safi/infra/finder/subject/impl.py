4474817a1d15537ce44ac32ebb9b5267490ffff1
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-16 22:23:40 +0200

    qsa: Update SubjectFinderdocumentation

--- safi/infra/finder/subject/impl.py
+++ safi/infra/finder/subject/impl.py
@@ -4,4 +4,8 @@ from .base import BaseSubjectFinder
 class SubjectFinder(BaseSubjectFinder):
 
     def available_challenges(self, gsid):
+        """Returns a Data Transfer Object (DTO) containing the factors that are
+        available for interim authentication challenges against the Subject
+        identified by string `gsid`.
+        """
         raise NotImplementedError("Subclasses must override this method.")
