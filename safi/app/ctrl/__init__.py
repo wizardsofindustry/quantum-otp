8fe1749dd9fc5c11b42aaa1055c8e94512560064
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-16 21:00:00 +0000

    qsa: Compile top-level controllers package.

--- safi/app/ctrl/__init__.py
+++ safi/app/ctrl/__init__.py
@@ -1,5 +1,5 @@
 """Contains all controller implementations that are used by the WSGI
-appication. See also :class:`~otp.app.wsgi.application.WSGIApplication`.
+appication. See also :class:`~safi.app.wsgi.application.WSGIApplication`.
 """
 from .onetimepassword import OneTimePasswordCtrl
 from .verification import VerificationCtrl
