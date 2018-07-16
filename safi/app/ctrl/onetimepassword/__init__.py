5f79ad268d67154676f9d75d59a96ab4670a5c53
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-16 21:00:00 +0000

    qsa: Compile OneTimePasswordCtrl

--- safi/app/ctrl/onetimepassword/__init__.py
+++ safi/app/ctrl/onetimepassword/__init__.py
@@ -1,4 +1,4 @@
-"""The :mod:`~otp.app.ctrl.onetimepassword` module holds the base
+"""The :mod:`~safi.app.ctrl.onetimepassword` module holds the base
 class and concrete implementation of :class:`OneTimePasswordCtrl`.
 """
 from .base import BaseOneTimePasswordCtrl
