1ebac2bc3cd87bb0a15e95a0a66d21a349b39305
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-16 22:20:01 +0200

    Implement SubjectChallengeCtrl.get()

--- safi/app/ctrl/subjectchallenge/impl.py
+++ safi/app/ctrl/subjectchallenge/impl.py
@@ -4,5 +4,6 @@ from .base import BaseSubjectChallengeCtrl
 
 class SubjectChallengeCtrl(BaseSubjectChallengeCtrl):
 
-    async def get(self, request, *args, **kwargs):
-        raise NotImplementedError("Subclasses must override this method.")
+    async def get(self, request, gsid, *args, **kwargs):
+        dto = {'challenges': self.subject.available_challenges(gsid)}
+        return self.render_to_response(dto)
