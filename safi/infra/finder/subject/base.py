35ed2c55bf31070277e2f9c8e68194ad4466deea
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-17 00:33:00 +0200

    qsa: Compile SubjectFinder

--- safi/infra/finder/subject/base.py
+++ safi/infra/finder/subject/base.py
@@ -1,8 +1,10 @@
+import ioc
 from sq.readmodel import Finder
 from sq.exceptions import ObjectDoesNotExist
 
 
 class BaseSubjectFinder(Finder):
+    session = ioc.class_property('DatabaseSessionFactory')
 
     NoChallengesFound = type('NoChallengesFound', (ObjectDoesNotExist,), {})
 
