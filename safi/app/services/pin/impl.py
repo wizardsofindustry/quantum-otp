f4af3fd9181c2a0de12b25332d7c20f7fc3545dc
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-09-08 19:55:47 +0200

    Implement PinService.createpin()

--- safi/app/services/pin/impl.py
+++ safi/app/services/pin/impl.py
@@ -1,10 +1,22 @@
+"""Declares the :class`PinService`."""
+import secrets
+
 from .base import BasePinService
+
+
+random = secrets.SystemRandom()
 
 
 class PinService(BasePinService):
 
     def createpin(self, gsid, pin=None):
-        raise NotImplementedError("Subclasses must override this method.")
+      """Create a new PIN for the **Subject** identified by `gsid`. If
+      `pin` is ``None``, then a code is automatically generated.
+      """
+      if pin is None:
+        pin = random.randint(10000, 99999)
+      self.repo.persist(self.dto(gsid=gsid, pin=pin))
+      return self.dto(pin=pin)
 
     def verify(self, gsid, pin):
         raise NotImplementedError("Subclasses must override this method.")
