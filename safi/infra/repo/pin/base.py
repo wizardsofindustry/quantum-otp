43d81afc4b852dc70768c2fa8c7b039fcda7832a
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-09-08 20:34:31 +0200

    qsa: Compile PinRepository

--- safi/infra/repo/pin/base.py
+++ safi/infra/repo/pin/base.py
@@ -5,6 +5,9 @@ from sq.persistence import Repository
 class BasePinRepository(Repository):
     session = ioc.class_property('DatabaseSessionFactory')
 
+    def get(self, gsid):
+        raise NotImplementedError("Subclasses must override this method.")
+
     def exists(self, gsid):
         raise NotImplementedError("Subclasses must override this method.")
 
